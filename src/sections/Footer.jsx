0fa977d96a62eb8de37d666b429a589b6a41f54d
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-09-13 15:21:29 -0500

    continuing to work on footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,10 +1,10 @@
-import { footerLogo, footerLinks } from "../assets/images";
-import { socialMedia } from "../constants"
+import { footerLogo } from "../assets/images";
+import { socialMedia, footerLinks } from "../constants"
 
 const Footer = () => {
   return (
     <footer className="max-container">
-      <div className="flext justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
           <a href="/">
             <img 
@@ -13,10 +13,10 @@ const Footer = () => {
               height={46}
             />
           </a>
-          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-screen-sm">Get shoes ready for the new term at your nearest Nike store. Find your perfect size in store. Get rewards.</p>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">Get shoes ready for the new term at your nearest Nike store. Find your perfect size in store. Get rewards.</p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+              <div key={icon} className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
                 <img
                   src={icon.src}
                   alt={icon.alt}
@@ -30,8 +30,24 @@ const Footer = () => {
 
         <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
           {footerLinks.map((section) => (
-
+            <div key={section}>
+              <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">{section.title}</h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li key={link.name} className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer">
+                    <a>
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           ))}
+        </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div>
+          
         </div>
       </div>
     </footer>
